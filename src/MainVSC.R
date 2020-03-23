f5f0b4a3b2bc4d35747ec085083c070101c0ef92
ATorneri <Atorneri@ATorneris-MacBook-Pro.local>
2020-03-23 11:28:48 +0100

    Update with a function that include asymptomatic individuals

--- src/MainVSC.R
+++ src/MainVSC.R
@@ -9,7 +9,7 @@ setwd(wd)
 library(foreach)
 library(doParallel)
 # load functions
-source("simFunctions.R")
+source("SimulationFunctions.R")
 # Parameters
 n<-500 #Population size
 lambda<-12 #daily rate of social contacts
@@ -22,8 +22,7 @@ mu.IP<-18 #28.6 #2.3 NEJM Li (SerialInt-Inc) -Anne Cori - SARS
 R.s<-2.5 # reproduction number symptomatic (mild)
 R.ss<-2.5 # reproduction number severe symptomatic
 R.a<-R.s #reproduction number asymptomatic
-tqs<-1.6
-tqss<-1.6
+avg.dt<-1.6
 t.zero<-6
 eta<-0.5
 tfailtest<-2
@@ -34,7 +33,7 @@ VShed<-"After"
 cl<-makeCluster(cores)
 registerDoParallel(cl)
 
-nSim<-5000
+nSim<-4
 epi.outbreak<-list()
 nSeed<-14022020
 set.seed(nSeed)
@@ -43,19 +42,12 @@ epi.outbreak<-foreach(i = 1:nSim) %dopar%{
   #epi.outbreak[[i]]<-nCov.simulator.ExpQuar (n=n, lambda = lambda, pbq.s = pbq.s, pbq.ss = pbq.ss, rho = rho, sigma = sigma, avg.inc = avg.inc, mu.IP = mu.IP, R.a = R.a, R.s=R.s, R.ss=R.ss,tqs=tqs,tqss = tqss, rq=rq)
   #epi.outbreak[[i]]<-nCov.simulator.GamIso(n=n, lambda = lambda, pbq.s = pbq.s, pbq.ss = pbq.ss, rho = rho, sigma = sigma, avg.inc = avg.inc, mu.IP = mu.IP, R.a = R.a, R.s=R.s, R.ss=R.ss,tqs=tqs,tqss = tqss)
   #epi.outbreak[[i]]<-nCov.simulator.ConstQuar(n=n, lambda = lambda, pbq.s = pbq.s, pbq.ss = pbq.ss, rho = rho, sigma = sigma, avg.inc = avg.inc, mu.IP = mu.IP, R.a = R.a, R.s=R.s, R.ss=R.ss,tqs=tqs,tqss = tqss)
-  epi.outbreak[[i]]<-nCov.simulator.ExpHosp.QuarIso.cttracing.aftersympt.2nd.asymptm (n=n, lambda = lambda, pbq.s = pbq.s, pbq.ss = pbq.ss, rho = rho, sigma = sigma, avg.inc = avg.inc, mu.IP = mu.IP, R.a = R.a, R.s=R.s, R.ss=R.ss,tqs=tqs,tqss = tqss, rate.quar = lambda.dec, eta=eta, t.zero = t.zero)  
+  epi.outbreak[[i]]<-nCov.simulator.ExpHosp.QuarIso.cttracing.aftersympt.asymptm (n=n, lambda = lambda, rho = rho, sigma = sigma, avg.inc = avg.inc, mu.IP = mu.IP, R.a = R.a, R.s=R.s,avg.dt=avg.dt, rate.quar = lambda.dec, eta=eta)  
   #epi.outbreak[[i]]<-nCov.simulator.ExpHosp.QuarIsoTreat.cttracing.beforesympt.delaytesting.asymptm(n=n, lambda = lambda, pbq.s = pbq.s, pbq.ss = pbq.ss, rho = rho, sigma = sigma, avg.inc = avg.inc, mu.IP = mu.IP, R.a = R.a, R.s=R.s, R.ss=R.ss,tqs=tqs,tqss = tqss, rate.quar = lambda.dec, eta=eta, t.zero = t.zero, tfailtest = tfailtest, tdelaytest = tdelaytest)  
   
   #print(i)
 }
 stopCluster(cl)
-
-#setwd("~/Desktop/Ncov/PaperSIGT/Simulations/Varying_TimeToHosp")
-#setwd("~/Desktop/Ncov/PaperSIGT/Simulations/VaryingIP")
-#setwd("~/Desktop/Ncov/PaperSIGT/Simulations/VaryingAsymptmProp")
-#setwd("~/Desktop/Ncov/PaperSIGT/Simulations/VaryingQuar")
-#setwd("~/Desktop/Ncov/PaperSIGT/Simulations/VaryingRA")
-
 
 
 name<-paste("Asymptomatic",n,"_propUnd",rho,"_Rs",R.s,"_Ra",R.a,"_lambda",lambda,"_Scenario",VShed,"_eta",eta,"_lambda_dec",lambda.dec,"_tfail",tfailtest,"_.RData", sep = "")
